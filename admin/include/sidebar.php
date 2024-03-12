5c79be1311e6921ff8c7864248b034a0ec5034d3
Rachit <rachitshahi001@gmail.com>
2024-03-12 19:43:28 +0545

    Parking 1st commit

---
+++ admin/include/sidebar.php
@@ -0,0 +1,92 @@
+<aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 " id="sidenav-main">
+    <div class="sidenav-header">
+      <a class="navbar-brand m-0" href="index.php">
+    <h4>EasyPark</h4>  
+    </a>
+    </div>
+
+    <hr class="horizontal dark mt-0">
+    <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
+      
+    <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link  active" href="report.php">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="fa fa-home text-white text-lg"></i>
+            </div>
+            <span class="nav-link-text ms-1">Dashboard</span>
+          </a>
+        </li>
+        <li class="nav-item mt-3">
+          <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Reports</h6>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link  " href="report.php">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+            <i class="fa fa-bullhorn text-dark text-lg"></i>
+            </div>
+            <span class="nav-link-text ms-1">Parking reports</span>
+          </a>
+        </li>
+
+        <li class="nav-item mt-3">
+          <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Manage Services</h6>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link  " href="services.php">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+            <i class="fa fa-tools text-dark text-lg"></i>
+            </div>
+            <span class="nav-link-text ms-1">Services</span>
+          </a>
+        </li>
+
+        <li class="nav-item mt-3">
+          <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Site management</h6>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link  " href="users.php">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+            <i class="fa fa-user-plus text-dark text-lg"></i>
+            </div>
+            <span class="nav-link-text ms-1">Admin/Users</span>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link  " href="category.php">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+            <i class="fa fa-globe text-dark text-lg"></i>
+            </div>
+            <span class="nav-link-text ms-1">Manage Category</span>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link  " href="Settings.php">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+            <i class="fa fa-cogs text-dark text-lg"></i>
+            </div>
+            <span class="nav-link-text ms-1">Settings</span>
+          </a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link  " href="Vehicle.php">
+            <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
+            <i class="fa fa-car text-dark text-lg"></i>
+            </div>
+            <span class="nav-link-text ms-1">Vehicle Entry</span>
+          </a>
+        </li>
+
+    </div>
+    <div class="sidenav-footer mx-3 ">
+      <a class="btn bg-gradient-primary mt-3 w-100" href="logout.php">
+        Logout
+    </a>
+    </div>
+  </aside>
---
+++ admin/users.php
@@ -0,0 +1,45 @@
+<?php include('include/header.php'); ?>
+
+<div class="row"></div>
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-header">
+            <h4>
+                Users List
+                <a href="users-create.php" class="btn btn-primary float-end">Add User</a>
+            </h4>
+        </div>
+        <div class="card-body">
+
+        <table class="table table-bordered table-stripped">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Username</th>
+                    <th>FullName</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>username</td>
+                    <td>name</td>
+                    <td>email</td>
+                    <td>phone</td>
+                    <td>
+                        <a href="users-edit.php" class="btn btn-success btn-sm">Edit</a>
+                        <a href="users-delete.php" class="btn btn-danger btn-sm mx-2">Delete</a>
+                    </td>
+
+                </tr>
+            </tbody>
+        </table>
+
+        </div>
+    </div>
+</div>
+
+<?php include('include/footer.php'); ?>
